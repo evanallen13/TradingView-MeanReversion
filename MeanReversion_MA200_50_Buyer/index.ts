3b4c8d857866ca0a712285504bc6ce8aa6dab74e
evanallen13 <evanallen13@gmail.com>
2021-10-25 02:55:09 +0000

    fix

--- MeanReversion_MA200_50_Buyer/index.ts
+++ MeanReversion_MA200_50_Buyer/index.ts
@@ -5,7 +5,7 @@ import typePrice from "../src/Models/typePrice";
 import typeCoin from "../src/Models/typeCoin";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    
+
     const ticker: string = req.query.ticker; //req.body.ticker;
     const kraken: Kraken = new Kraken();
     const coin: typeCoin = Coins[ticker];
@@ -14,7 +14,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const askBid: typePrice = await kraken.AskBid(coin);
 
     if (coin.Minimum * askBid.Ask < currBalance) {
-
+        context.log("Buy");
     }
 
     const responseMessage = `Ther price of ${coin.KrakenPair} is $${askBid.Ask}`;
