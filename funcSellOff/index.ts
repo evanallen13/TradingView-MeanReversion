31daaec4e80065f94a0541f7e9261a9311388465
evanallen13 <evanallen13@gmail.com>
2021-11-09 19:05:57 -0800

    add Sell Off

--- funcSellOff/index.ts
+++ funcSellOff/index.ts
@@ -3,6 +3,7 @@ import Kraken from "../src/Kraken";
 import Data from "../src/Data";
 import Coins from "../src/Coins";
 import typeCoin from "../src/Models/typeCoin";
+import typeOrder from "../src/Models/typeOrder";
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     
@@ -13,9 +14,35 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
     for (let buy of buys) {
         const coin: typeCoin = Coins[buy.Ticker];
         const price = (await kraken.AskBid(coin)).Bid;
-        console.log(buy);
+        
         if(price >= buy.SellOff){
-            console.log("Sell Off");
+            const currBalance = await kraken.AskBid(coin);
+            const txid: string = await kraken.AddOrder({
+                pair: coin.KrakenPair,
+                type: "sell",
+                ordertype: "market",
+                price: currBalance.Bid,
+                volume: buy.Volume
+            });
+
+            const krakenOrder = await kraken.QueryOrder(txid, coin);
+
+           const order: typeOrder = {
+                ticker: coin.KrakenPair,
+                txid: txid,
+                pair: coin.Pair,
+                volume: Number(buy.vol),
+                price: buy.Volume,
+                sellPrice: krakenOrder.sellPrice,
+                createdOn: new Date(),
+                fee: krakenOrder.fee,
+                type: "sell",
+                ordertype: "market",
+                buyerTxid: buy.TXID,
+                sellType: "SellOff",
+            }
+
+            const res = data.Seller_Save(order);
         }
     }
 };
